db5d9751d3882eb2ff68e6989f79aba26e1ba1ce
Nuha <azazi.nuha@gmail.com>
2022-09-23 00:52:17 +0200

    style,add error function andlanguages

--- app.js
+++ app.js
@@ -13,25 +13,57 @@ async function fetchAndTranslate(text, targetLanguage) {
 	);
 
 	console.log(data);
-    return data
+	return data;
+}
+
+async function minionsTranslate(text, targetLanguage) {
+	const data = await fetchData(``);
+
+	console.log(data);
+	return data;
+}
+
+function renderError(error) {
+	// const h1 = document.createElement("h1");
+    const errorDiv = document.querySelector("#error");
+	errorDiv.textContent = error;
+	// document.body.appendChild(h1);
+
+	console.log(error);
 }
 
 function main() {
-	let textTo = document.querySelector(".textTo");
+	const select = document.querySelector("select");
+	const textTo = document.querySelector(".textTo");
 	const translateBtn = document.querySelector(".button");
-	let textForm = document.querySelector(".textForm");
-	translateBtn.addEventListener("click", async() => {
+	const textForm = document.querySelector(".textForm");
+	translateBtn.addEventListener("click", async () => {
 		let text = textForm.value;
-// let translation = textTo.value
-		// console.log(translation);
-        const translation = await fetchAndTranslate(text,"nl");
-        textTo.value=translation.responseData.translatedText
-        // console.log(translation.responseData["translatedText"])
+		let targetLanguage = select.value;
 
+		if (textForm.value) {
+			try {
+				const translation = await fetchAndTranslate(text, targetLanguage);
+				textTo.value = translation.responseData.translatedText;
+			} catch (error) {
+				renderError(error.message);
+			}
+		}else{
+            renderError("YOU HAVE TO ENTER TEXT")
+        }
+
+		// console.log(translation.responseData["translatedText"])
+		// const translation1 = await fetchAndTranslate(text,"fr");
+		// textTo.value=translation1.responseData.translatedText
+
+		// const translation2 = await fetchAndTranslate(text,"es");
+		// textTo.value=translation2.responseData.translatedText
 	});
+	// button.addEventListener("change", async(e) => {
+	//    textTo =e.target.value
+	//     console.log(e.target.value)
 
-	
-	
+	// });
 }
 
 window.addEventListener("load", main);
